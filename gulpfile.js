3a35f37955ff58ef7b22bd43d3d7e7e28f4d89ba
Maksim Smirnov <maxinflame@yandex.ru>
2023-09-23 12:45:18 +0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
 const rename = require('gulp-rename');
+const sync = require("browser-sync").create();
+
 
 const styles = () => {
   return gulp.src("sass/style.scss")
   .pipe(sass())
   .pipe(gulp.dest("css/"))
 }
+
+const server = () => {
+  sync.init({
+    server: {
+        baseDir: "./"
+    }
+});
+}
+
 
 exports.styles = styles;
 
@@ -25,4 +36,7 @@ const watcher = () => {
   ));
 }
 
-exports.watcher = watcher;
+exports.default = gulp.series(
+  server,
+  watcher,
+)
