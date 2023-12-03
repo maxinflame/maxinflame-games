f2cc03187ef430275566a24edf2fd7d5a14270a1
Maksim Smirnov <maxinflame@yandex.ru>
2023-12-03 14:54:37 +0300

    add pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,41 +3,59 @@ const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
 const rename = require('gulp-rename');
 const sync = require("browser-sync").create();
+const pug = require('gulp-pug');
 
 
 const styles = () => {
-  return gulp.src("sass/style.scss")
+  return gulp.src("src/sass/style.scss")
   .pipe(sass())
-  .pipe(gulp.dest("css/"))
+  .pipe(gulp.dest("dist/css/"))
 }
 
-const server = () => {
+const server = (done) => {
   sync.init({
     server: {
-        baseDir: "./"
+        baseDir: "./dist"
     }
-});
+  });
+  done();
 }
 
 
 exports.styles = styles;
 
 const mincss = () => {
-    return gulp.src("css/style.css")
+    return gulp.src("dist/css/style.css")
     .pipe(csso())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(sync.stream());
 }
 
-const watcher = () => {
-  gulp.watch("sass/**/*.scss", gulp.series(
-    styles,
-    mincss,
-  ));
+const createHtml = () => {
+  return gulp.src('./src/pug/pages/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./dist'))
 }
 
-exports.default = gulp.parallel(
-  watcher,
-  server,
-)
+
+const watcher = () => {
+  gulp.watch("src/sass/**/*.scss", gulp.series(
+    styles,
+    mincss,
+  ))
+
+  gulp.watch("src/pug/**/*.pug", gulp.series(
+    createHtml,
+  ))
+
+  gulp.watch('dist/*.html').on('change', gulp.series(sync.reload))
+}
+
+const start = gulp.series(
+  gulp.parallel(styles, createHtml),
+  gulp.parallel(watcher, server)
+);
+
+exports.default = start;
+
