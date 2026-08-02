7828c7f5a959795936897bfa818334d3d4ac057e
Maksim Smirnov <maxinflame@yandex.ru>
2026-08-02 22:38:21 +0300

    remove gulp

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,69 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
   resolve: {
     extensions: ['.js', '.json'],
     extensionAlias: {
       '.js': ['.js', '.mjs', '.cjs'],
     },
   },
+
   entry: {
     life: './src/js/life.js',
     snake: './src/js/snake.js',
     tetris: './src/js/tetris.js',
+    style: './src/scss/style.scss',
   },
+
   output: {
     filename: '[name].js',
-    path: __dirname,
+    path: path.resolve(__dirname, 'www/js'),
   },
+
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '../img/[name][ext]',
+        },
+      },
+
+      {
+        test: /\.(woff|woff2|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '../fonts/[name][ext]',
+        },
       },
     ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css',
+    }),
+  ],
 };
